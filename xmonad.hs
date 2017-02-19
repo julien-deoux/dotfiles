e0b7eb984553d7598b0dabf03e4a97c9b6f9699f
Julien Déoux <julien.deoux@telecomnancy.net>
2017-02-19 15:03:06 +0100

    [~] Key bindings

--- xmonad.hs
+++ xmonad.hs
@@ -3,10 +3,11 @@ import XMonad.Config.Azerty
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Spacing
+import XMonad.Layout.BinarySpacePartition
 import System.IO
 
 baseConfig = azertyConfig
@@ -20,12 +21,21 @@ lemakiPP = xmobarPP
 	, ppLayout = const ""
 	}
 
+lemakeys = 
+	[ ((mod4Mask .|. shiftMask, xK_n), spawn "chromium")
+	, ((mod4Mask .|. shiftMask, xK_f), spawn "thunar")
+	, ((mod4Mask, xK_Pause), spawn "i3lock -i /home/julien/Pictures/Wallpapers/i3lock.png && sleep")
+	, ((mod4Mask .|. shiftMask, xK_Pause), spawn "poweroff")
+	, ((0, 0x1008FF02), spawn "xbacklight -inc 5") -- xF86XK_MonBrightnessUp
+	, ((0, 0x1008FF03), spawn "xbacklight -dec 5") -- xF86XK_MonBrightnessDown
+	]
+
 main = do
 	xmproc <- spawnPipe "/bin/xmobar /home/julien/.xmobarrc"
 
 	xmonad $ docks $ fullscreenSupport $ baseConfig
 		{ manageHook = manageDocks <+> manageHook baseConfig
-		, layoutHook = avoidStruts $ spacingWithEdge 4 $ layoutHook baseConfig
+		, layoutHook = avoidStruts $ spacingWithEdge 4 $ emptyBSP
 		, logHook = dynamicLogWithPP lemakiPP
 			{ ppOutput = hPutStrLn xmproc
 			, ppTitle = xmobarColor "#6ca90e" "" . shorten 50
@@ -33,4 +43,4 @@ main = do
 		, terminal = "termite"
 		, modMask = mod4Mask
 		, borderWidth = 0
-		}
+		} `additionalKeys` lemakeys
