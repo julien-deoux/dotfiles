721383ab9f5b2f5c675a8b0e3da495b63dc7c179
Julien Déoux <julien.deoux@telecomnancy.net>
2017-02-24 18:11:35 +0100

    [~] Fixed faulty install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-DOTFILES_DIR=$(PWD)
+DOTFILES_DIR=$(pwd)
 
-ln -s $DOTFILES_DIR/xinitrc ~/.xinitrc
-ln -s $DOTFILES_DIR/bashrc ~/.bashrc
-ln -s $DOTFILES_DIR/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s $DOTFILES_DIR/xmobarrx ~/.xmobarrc
-ln -s $DOTFILES_DIR/termite ~/.config/termite/config
-ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
-ln -s $DOTFILES_DIR/vimrc ~/.vimrc
-ln -s $DOTFILES_DIR/vim ~/.vim
+ln -sf $DOTFILES_DIR/xinitrc ~/.xinitrc
+ln -sf $DOTFILES_DIR/bashrc ~/.bashrc
+ln -sf $DOTFILES_DIR/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf $DOTFILES_DIR/xmobarrx ~/.xmobarrc
+ln -sf $DOTFILES_DIR/termite ~/.config/termite/config
+ln -sf $DOTFILES_DIR/gitconfig ~/.gitconfig
+ln -sf $DOTFILES_DIR/vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/vim ~/.vim
 
